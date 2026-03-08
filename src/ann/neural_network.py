24a544491f3062bd948add578b285abf80fa03ee
NandhiniM <nandhinim644@gmail.com>
2026-03-08 13:09:46 +0530

    update

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -82,6 +82,7 @@ class NeuralNetwork:
         Returns:
             return grad_w, grad_b
         """
+        self.loss_fn.forward(y_pred, y_true)
         # Compute loss gradient
         dA = self.loss_fn.backward(y_pred, y_true)
         
